ea8730e8ce203b7d8ef0fb03cd66647d4c6688c7
Ruthvik Kamarasu <kamarasuruthvik@gmail.com>
2024-09-18 08:33:51 -0700

    Initial Commit

---
+++ demoios/demoiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoiosApp.swift
+//  demoios
+//
+//  Created by ruthvikkamarasu on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct demoiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ demoiosTests/demoiosTests.swift
@@ -0,0 +1,17 @@
+//
+//  demoiosTests.swift
+//  demoiosTests
+//
+//  Created by ruthvikkamarasu on 18/09/24.
+//
+
+import Testing
+@testable import demoios
+
+struct demoiosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
